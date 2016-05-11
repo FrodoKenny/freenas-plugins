ba9c5b343d0702b1aa1f7aef4de1b84811e394d4
FrodoKenny <frodokenny@hotmail.com>
2016-05-11 09:40:37 +0100

    Update crashplan plugin

--- plugins/crashplan/scripts/post-install.sh
+++ plugins/crashplan/scripts/post-install.sh
@@ -5,12 +5,23 @@ crashplan_pbi_path=/usr/pbi/crashplan-$(uname -m)
 
 /bin/cp ${crashplan_pbi_path}/etc/rc.d/crashplan /usr/local/etc/rc.d/
 
-mkdir -p /compat/linux
-ln -sf ${crashplan_pbi_path}/linuxlib /compat/linux/lib
+ln -sf ${crashplan_pbi_path}/compat /compat
+if [ -d "${crashplan_pbi_path}/share/crashplan/log" ]; then
+	mv ${crashplan_pbi_path}/share/crashplan/log /var/log/crashplan
+else
+	mkdir /var/log/crashplan
+fi
+ln -sf /var/log/crashplan ${crashplan_pbi_path}/share/crashplan/log
+if [ -d "${crashplan_pbi_path}/share/crashplan/cache" ]; then
+	mv ${crashplan_pbi_path}/share/crashplan/cache /var/cache/crashplan
+else
+	mkdir /var/cache/crashplan
+fi
+ln -sf /var/cache/crashplan ${crashplan_pbi_path}/share/crashplan/cache
 
 #For some reason the default relative RPATH doesnt work in java bin
 ${crashplan_pbi_path}/bin/patchelf \
-	--set-rpath ${crashplan_pbi_path}/linux-sun-jre1.6.0/lib/i386/jli \
-	${crashplan_pbi_path}/linux-sun-jre1.6.0/bin/java
+	--set-rpath ${crashplan_pbi_path}/linux-sun-jre1.7.0/lib/i386/jli \
+	${crashplan_pbi_path}/linux-sun-jre1.7.0/bin/java
 
-${crashplan_pbi_path}/bin/python ${crashplan_pbi_path}/crashplanUI/manage.py syncdb --migrate --noinput
+${crashplan_pbi_path}/bin/python2.7 ${crashplan_pbi_path}/crashplanUI/manage.py syncdb --migrate --noinput
